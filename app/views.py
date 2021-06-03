a9255420a7ea86e4eee7e89819a5f2d5fee641bc
Guil-Martin <gu.martin@yahoo.com>
2021-06-03 10:48:45 +0200

    Added a login page with redirection to index

--- app/views.py
+++ app/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Student
 
+from django.contrib.auth import authenticate, login
+
 # Create your views here.
+
+def login_page(request):
+    if request.method == 'POST':
+        # Get info from form in templates.login_register.html
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use django authenticate function to ckeck if user exists
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # If user exists use django login function
+            login(request, user)
+            return redirect('index')    # Redirect to the route index by its name 
+                                        # return so it dosen't render and don't redirect
+
+    return render(request, "login_register.html")
+    
 def index(request):
     obj=Student.objects.all()
     context={
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import index
+from app.views import index, login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index,name="index")
+    path('login/', login_page, name="login"),
+    path('', index, name="index")
 ]
